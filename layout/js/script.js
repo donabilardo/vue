eb6d7ff43d05c0c2f7e8ad0394efadb1c5b316ec
Don Abilardo <don.abilardo@gmail.com>
2023-11-05 09:46:35 +0500

    Панель навигации

--- layout/js/script.js
+++ layout/js/script.js
@@ -1,12 +1,25 @@
 new Vue({
     el: '#app',
     data: {
-        test: 'TEST111'
+        logoImg: 'img/logo.svg',
+        siteName: 'GB - VUE.js',
+        navbar:[
+            {
+                name: 'Home',
+                link: '/'
+            },
+            {
+                name: 'Project',
+                link: '#'
+            },
+            {
+                name: 'Blog',
+                link: '#'
+            },
+        ]
     },
     methods: {
-        bum() {
-            alert('BUMMMMMM!!!!!!')
-        }
+
     },
     computed: {
 
